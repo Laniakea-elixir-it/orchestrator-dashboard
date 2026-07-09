694b764b17489e8d969cde8af8b64c4719790ab4
mtangaro <ma.tangaro@gmail.com>
2026-07-09 15:47:52 +0000

    add javascrpt

--- app/deployments/static/js/script.js
+++ app/deployments/static/js/script.js
@@ -1,4 +1,4 @@
-
+// Toggles visibility of dependent form fields
 function hideOrShow(el) {
   var fname = hideOrShow.name.toLowerCase();
   var selector = $(el).data(fname + '-selector');
@@ -17,3 +17,34 @@ function hideOrShow(el) {
     }
   });
 };
+
+// Check password double time.
+function checkSecretMatch(input) {
+  var confirmInput = document.getElementById(input.id + '_confirm');
+  var msg = document.getElementById(input.id.replace('_confirm', '') + '_match_msg');
+  if (!confirmInput || !msg) return;
+
+  var baseInput = document.getElementById(input.id.replace('_confirm', ''));
+
+  if (!confirmInput.value) {
+    msg.textContent = '';
+    msg.classList.remove('text-danger');
+    confirmInput.setCustomValidity('');
+    return;
+  }
+  if (baseInput.value !== confirmInput.value) {
+    msg.textContent = 'Passwords do not match.';
+    msg.classList.add('text-danger');
+    confirmInput.setCustomValidity('Passwords do not match.');
+  } else {
+    msg.textContent = 'Passwords match.';
+    msg.classList.remove('text-danger');
+    confirmInput.setCustomValidity('');
+  }
+}
+
+$(document).on('input', 'input[data-type="secret"]', function () {
+  var id = this.id.endsWith('_confirm') ? this.id.replace('_confirm', '') : this.id;
+  var mainInput = document.getElementById(id);
+  checkSecretMatch(mainInput);
+})
